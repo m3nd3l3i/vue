8aecc5cfd85b1dcc87de5a31ffe098f924a6695f
dmitrij-inbank <107244962+dmitrij-inbank@users.noreply.github.com>
2023-06-05 16:44:32 +0300

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img id="nav-logo" src="../assets/logo.png" alt="Vue Logo" width="30" height="30">
+      </a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li v-for="(page, index) in pages" class="nav-item" :key="index">
+            <navbar-link
+                :page="page"
+                :isActive="activePage === index"
+                @click.prevent="navLinkClick(index)"
+            ></navbar-link>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button class="btn btn-primary" @click.prevent="changeTheme()">Toggle</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+import NavbarLink from "@/components/NavbarLink.vue";
+export default {
+  components: {
+    NavbarLink
+  },
+  created() {
+    this.getThemeSettings();
+  },
+  props: ['pages', 'activePage', 'navLinkClick'],
+  data() {
+    return {
+      theme: 'light',
+    }
+  },
+  methods: {
+    changeTheme() {
+      let theme = 'light';
+      if (this.theme == 'light') {
+        theme = 'dark';
+      }
+      this.theme = theme;
+      this.storeThemeSettings();
+    },
+    storeThemeSettings() {
+      localStorage.setItem('theme', this.theme);
+    },
+    getThemeSettings() {
+      let theme = localStorage.getItem('theme');
+      if (theme) {
+        this.theme = theme;
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/PageViewer.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="emphasize">{{page.pageTitle}}</h1>
+    <p>{{page.content}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['page']
+}
+</script>
+<style scoped>
+  .emphasize {
+    color: forestgreen;
+  }
+</style>
