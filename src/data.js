37eddc5a53d6dab855862ea2dc9c08d3d86fdbce
dmitrij-inbank <107244962+dmitrij-inbank@users.noreply.github.com>
2023-07-03 10:01:05 +0300

    all

--- src/data.js
+++ src/data.js
@@ -3,11 +3,19 @@ const pagesKey = 'pages';
 let pagesJson = localStorage.getItem(pagesKey);
 let pagesStore = JSON.parse(pagesJson);
 
+/**
+ * Save updated page list to local cache
+ */
 function save() {
     localStorage.setItem(pagesKey, JSON.stringify(pagesStore));
 }
 
 export default {
+
+    /**
+     * Load pages from local browser cache storage OR from pages.json to local browser cache storage
+     * @returns {Promise<void>}
+     */
     async loadPages() {
         if (pagesStore === null) {
             let res = await fetch('pages.json');
@@ -16,16 +24,29 @@ export default {
         }
     },
 
+    /**
+     * Add new page to local cache
+     * @param page
+     */
     addPage(page) {
         pagesStore.push(page);
         save();
     },
 
+    /**
+     * get all pages from local cache
+     * @returns {any}
+     */
     getAllPages()
     {
         return pagesStore;
     },
 
+    /**
+     * get single page from local cache
+     * @param url
+     * @returns {null}
+     */
     getSinglePage(url) {
         let out = null;
         pagesStore.forEach((page, index) => {
@@ -36,6 +57,11 @@ export default {
             return out;
         },
 
+    /**
+     * Edit page in local cache
+     * @param url
+     * @param editedPage
+     */
     editPage(url, editedPage) {
         pagesStore.forEach((page, index) => {
             if(page.link.url === url) {
@@ -45,6 +71,10 @@ export default {
         });
     },
 
+    /**
+     * remove page from local cache
+     * @param url
+     */
     removePage(url) {
         pagesStore.forEach((page, index) => {
             if(page.link.url === url) {
